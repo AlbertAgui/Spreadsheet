9575b9edc1a706b462b36533016e2da43c9fb9b0
bscuser <xd@2>
2023-05-25 21:11:28 +0200

    :star: first approach generate postfix

--- src/main/java/org/example/Formula.java
+++ src/main/java/org/example/Formula.java
@@ -52,6 +52,7 @@ public class Formula { //1 + 2-4 //The preference in order used to find could be
     }
 
 
+
     //Parsing
     private static Boolean is_numerical(String token) {
         return is_operand(token) || is_operator(token);
@@ -64,10 +65,15 @@ public class Formula { //1 + 2-4 //The preference in order used to find could be
     }
 
     public static Boolean is_operator(String token){
-        if(token.matches("[+-]") || token.matches("[*/]")){
-            return true;
-        }
-        return false;
+        return is_highp_operator(token) || is_lowp_operator(token);
+    }
+
+    public static Boolean is_lowp_operator(String token){
+        return token.matches("[+-]");
+    }
+
+    public static Boolean is_highp_operator(String token){
+        return token.matches("[*/]");
     }
 
     private static Boolean is_claw(String token) {
@@ -86,6 +92,10 @@ public class Formula { //1 + 2-4 //The preference in order used to find could be
             return true;
         }
         return false;
+    }
+
+    public static Boolean is_parseable() {
+        return is_num_balanced() && is_claw_balanced();
     }
 
     public static Boolean is_num_balanced(){ //Need to be improved, not just extended!!(use precedence order!)
@@ -156,13 +166,68 @@ public class Formula { //1 + 2-4 //The preference in order used to find could be
         }
     }
 
-    public static Boolean is_parseable(){
-        return is_num_balanced() && is_claw_balanced();
-    }
+
 
     //Generate postfix
     private static LinkedList<String> postfix;
-    public static void
+
+    private static Integer get_precedence(String token) {
+        if (is_closed_claw(token)) return 3;
+        if (is_highp_operator(token)) return 2;
+        if (is_lowp_operator(token)) return 1;
+        if (is_open_claw(token)) return 0;
+        return -1; //error, will be modified
+    }
+
+    public static void DisplayPostfix(){
+        System.out.println("postfix count:" + postfix.size());
+        postfix.forEach(pf ->{
+            System.out.println(pf);
+        });
+    }
+
+    public static void generate_postfix() {
+        postfix = new LinkedList<>();
+        Stack<String> aux_stack = new Stack<>();
+        for (int i = 0; i < tokens.size(); ++i) {
+            String next = tokens.get(i);
+            if(is_operand(next)) {
+                postfix.add(next);
+            }
+            else if (aux_stack.isEmpty()) {
+                aux_stack.push(next);
+            } else {
+                String top = aux_stack.peek();
+                if(get_precedence(next) == 0){ //open clause
+                    aux_stack.push(next);
+                } else if (get_precedence(next) == 3) { //close clause: pop and out until top is '('
+                    while (!top.equals("(")) { //equals or match??
+                        aux_stack.pop();
+                        postfix.add(top);
+                        if(!aux_stack.isEmpty()) { //should not be necessary
+                            top = aux_stack.peek(); //gets value
+                        } else {
+                            break;
+                        }
+                    }
+                    aux_stack.pop(); // pop "(", not add ")", See, no checked stack size
+                } else if(get_precedence(next) > get_precedence(top)) {
+                    aux_stack.push(next);
+                } else {
+                    while (get_precedence(next) <= get_precedence(top)) { //pop and out until top has lower precendence than next
+                        aux_stack.pop();
+                        postfix.add(top);
+                        if(!aux_stack.isEmpty()) { //should not be necessary
+                            top = aux_stack.peek(); //gets value
+                        } else {
+                            break;
+                        }
+                    }
+                    aux_stack.push(next);
+                }
+            }
+        }
+    }
 
     //Evaluate postfix
 }
