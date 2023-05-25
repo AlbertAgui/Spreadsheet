1b3039c60f0a1d3cd89a49bc2dd56aafd21baea6
bscuser <xd@2>
2023-05-25 22:53:10 +0200

    :start: add first approach of a test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,36 @@
 package org.example;
 
+import java.text.Normalizer;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Canvi, Hello and welcome!\n");
         Spreadsheet spreadsheet = new Spreadsheet();
-        String s_coordinate = "AA23";
-        Num_coordinate num_coordinate;
-
         spreadsheet = Load_store.loadspreadsheet("Data/t1.txt");
-
-        //spreadsheet.get_cell_value(num_coordinate);
-
-        if(Translate_coordinate.is_correct_coordinate(s_coordinate)) {
+        String s_coordinate = "A1";
+        if (Translate_coordinate.is_correct_coordinate(s_coordinate)) {
+            Num_coordinate num_coordinate;
             num_coordinate = Translate_coordinate.translate_coordinate_to_int(s_coordinate);
+            num_coordinate.display_coordinate();
 
-            System.out.println("col, row:" + num_coordinate.num_column + " , " + num_coordinate.num_row + "\n");
+            //compute formula value
+            Formula.setFormula_body("4+5*4/(3+6/5+3*(4*5))");
+            Formula.tokenize();
+            if (Formula.is_parseable()) {
+                System.out.println("Correct!");
+                Formula.generate_postfix();
+                float value = Formula.evaluate_postfix();
+                spreadsheet.set_cell_value(num_coordinate, value);
 
-            spreadsheet.set_cell_value(num_coordinate, 54);
-
-            //spreadsheet.set_cell_value("A1",23);
-            spreadsheet.get_cell_value(num_coordinate);
+                Load_store.storespreadsheet("Data/t2wr.txt", spreadsheet);
+            } else {
+                System.out.println("Incorrect!");
+            }
         } else {
             System.out.println("No valid coordinate\n");
         }
-
-        Load_store.storespreadsheet("Data/t2wr.txt", spreadsheet);
     }
 }
