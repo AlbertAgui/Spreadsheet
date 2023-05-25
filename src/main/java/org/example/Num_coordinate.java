13cfaef9b7fab61ac7eb603266b167fb4592032e
bscuser <xd@2>
2023-05-25 22:51:17 +0200

    :star: update num_coordinate

--- src/main/java/org/example/Num_coordinate.java
+++ src/main/java/org/example/Num_coordinate.java
@@ -1,6 +1,19 @@
 package org.example;
 
 public class Num_coordinate {
-    Integer num_column;
-    Integer num_row;
+    public Integer num_column;
+    public Integer num_row;
+
+    public Num_coordinate() {
+
+    }
+
+    public Num_coordinate(Integer num_row, Integer num_column) {
+        this.num_column = num_column;
+        this.num_row = num_row;
+    }
+
+    public void display_coordinate() {
+        System.out.println("coordinate: colum: " + num_column + " row: " + num_row);
+    }
 }
