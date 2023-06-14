5bf0da2d7594aa016db47b05e4be9cc82ea02505
bscuser <xd@2>
2023-06-14 19:45:21 +0200

    :hammer: load store corner cases

--- src/main/java/org/example/Load_store.java
+++ src/main/java/org/example/Load_store.java
@@ -4,13 +4,13 @@ import java.io.*;
 
 public class Load_store {
     static Spreadsheet loadspreadsheet(String s2vFilePath) {
+        BufferedReader reader = null;
         try {
             // Read the S2V file
-            BufferedReader reader = new BufferedReader(new FileReader(s2vFilePath));
+            reader = new BufferedReader(new FileReader(s2vFilePath));
             Spreadsheet spreadsheet = new Spreadsheet();
 
             String line;
-            //Integer cntRow = 0, cntColum = 0;
             Integer nRow = 1;
 
             while ((line = reader.readLine()) != null) {
@@ -18,7 +18,6 @@ public class Load_store {
                 String[] tokens = line.split(";");
                 Integer nColum = 1;
                 NumCoordinate numCoordinate = new NumCoordinate();
-                //if(cntColum == 0) cntColum = tokens.length;
 
                 // Create a new row
                 for (int i = 0; i < tokens.length; i++) {
@@ -32,34 +31,34 @@ public class Load_store {
                         content.setValue(t_value);
                         cell.setContent(content);
                         spreadsheet.cells.addCell(numCoordinate, cell);
-                    } else { //SHOULD BE CHANGED
-                        /*numCoordinate.setNumColum(nColum);
-                        numCoordinate.setNumRow(nRow);
-                        //System.out.println("load: colum: " + nColum + " row: " + nRow + " no value!\n");
-                        Cell cell = new Cell();
-                        Content content = new ContentNumerical(); //SHOULD BE CHANGED
-                        content.setValue(0);
-                        cell.setContent(content);
-                        spreadsheet.cells.addCell(numCoordinate, cell);*/
                     }
                     nColum++;
                 }
                 nRow++;
             }
-            //cntRow = nRow - 1;
-
-            /*NumCoordinate size = new NumCoordinate(cntRow, cntColum);
-            //System.out.println("Size: " + "colum: " +  cntColum + ", row: " + cntRow + "\n");
-            spreadsheet.setSize(size);*/
             return spreadsheet;
         } catch (IOException e) {
-            System.out.println("An error occurred while interpreting the S2V file: " + e.getMessage());
+            System.out.println("Error loading spreadsheet, path: \"" + s2vFilePath + "\", error message: " + e.getMessage());
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error closing spreadsheet reader" + ", error message: " + e.getMessage());
+            }
         }
         return null;
     }
 
 
     static void storespreadsheet(String s2vFilePath, Spreadsheet spreadsheet) {
+        if(spreadsheet == null) {
+            System.out.println("Error storing spreadsheet, invalid spreadsheet");
+            return;
+        }
+
+        BufferedWriter writer = null;
         try {
             // Read the S2V file
             File file = new File(s2vFilePath);
@@ -68,7 +67,7 @@ public class Load_store {
                 file.createNewFile();
             }
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer = new BufferedWriter(new FileWriter(file));
             String line;
 
             NumCoordinate size = spreadsheet.cells.getSize();
@@ -89,19 +88,23 @@ public class Load_store {
                         writer.write((String) value);
                     } else if (value instanceof Float) {
                         writer.write(Float.toString((Float) value));
-                    } /*else {
-                        //writer.write("");
-                        System.out.println("No data, row: " + i + " col: " + j);
-                    }*/
+                    } //MODIFY!! add something else??
                 }
                 if(i != nRow) {
                     writer.newLine();
                 }
             }
 
-            writer.close();
         } catch (IOException e) {
-            System.out.println("An error occurred while interpreting the S2V file: " + e.getMessage());
+            System.out.println("Error storing spreadsheet, path: \"" + s2vFilePath + "\", error message: " + e.getMessage());
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error closing spreadsheet writer" + ", error message: " + e.getMessage());
+            }
         }
     }
 }
