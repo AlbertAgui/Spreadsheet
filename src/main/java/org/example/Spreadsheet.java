1d6f0723a7abd86c08e432c7bea2d41891a98b27
Sobhan <Sobhan.fld2@gmail.com>
2023-06-17 11:51:04 +0200

    Spreadsheetprint added

--- src/main/java/org/example/Spreadsheet.java
+++ src/main/java/org/example/Spreadsheet.java
@@ -18,4 +18,18 @@ public class Spreadsheet {
     public NumCoordinate getSize() {
         return this.size;
     }
+
+    public void printSpreadsheet() {
+        for (int i = 1; i <= size.getNumRow(); i++) {
+            for (int j = 1; j <= size.getNumColum(); j++) {
+                Cell cell = cells.getCell(new NumCoordinate(i, j));
+                if (cell != null) {
+                    System.out.print("[ "+ cell.getContent().getValue()+ " ]");
+                } else {
+                    System.out.print("[ ] ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
